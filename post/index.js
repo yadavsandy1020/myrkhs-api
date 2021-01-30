67aa581fe2ed70d57a89b4901dd225ec033c265d
Sandeep Yadav <yadavsandy.1020@gmail.com>
2021-01-31 02:26:13 +0530

    fix: added policy to profile pic bucket

--- post/index.js
+++ post/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const uuid = require('uuid');
+const id = uuid.v4();
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-
+const s3Bucket = new AWS.S3({ params: { Bucket: process.env.PROFILE_BUCKET } });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const timestamp = new Date().getTime();
 
 module.exports.handler = (event, context, callback) => {
-  const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
-  console.log({body: event.body});
+
   if (typeof data.name !== 'string') {
     console.error('Validation Failed');
     callback(null, {
@@ -18,11 +19,60 @@ module.exports.handler = (event, context, callback) => {
     });
     return;
   }
+  if (!data.photo) {
+    return
+  }
+  saveProfilePhoto(id, data.photo).then(res => {
+    console.log({ res });
+    saveDataInDB(res.photo, data).then(params => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(params.Item),
+      };
+      return response
+    }, err => {
+      return {
+        statusCode: err.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: 'Couldn\'t create the member.',
+      };)
+  })
+}
 
+
+const saveProfilePhoto = async (uid, photo) => {
+  const buf = Buffer.from(photo.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+  const data = {
+    Key: uid,
+    Body: buf,
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg'
+  }
+  try {
+    await s3Bucket.putObject(data).promise()
+    return {
+      photo: `https://${process.env.PROFILE_BUCKET}.s3.amazonaws.com/${id}`
+    }
+  } catch (error) {
+    console.log({ error });
+    throw new Error('Error while saving picture')
+  }
+}
+
+
+const saveDataInDB = async (photo, data) => {
+  console.log('inside savedataindb');
   const params = {
     TableName: process.env.MEMBERS_TABLE,
     Item: {
-      id: uuid.v1(),
+      id,
+      photo,
       name: data.name,
       email: data.email,
       mobile: data.mobile,
@@ -33,27 +83,14 @@ module.exports.handler = (event, context, callback) => {
     },
   };
 
-  // write the member to the database
-  dynamoDb.put(params, (error) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the member.',
-      });
-      return;
-    }
+  try {
+    console.log('trying to save in db...')
+    await dynamoDb.put(params).promise();
+    console.log('saved!')
+    return params
+  } catch (error) {
+    console.log('problem in db put method');
+    throw new Error('problem in db put method')
+  }
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
-};
+}
